716e7bcf0da9d304bd9f9a65ccabefc2518b7cae
Kaye Bryant <bryantk72391@gmail.com>
2022-05-16 11:09:35 -0400

    backend set up and displaying json data

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+
+const about = require("./about.json");
+const categoriesList = require("./categoriesList.json");
+const timeManagement = require("./resourceList-TimeManagement.json");
+const goals = require("./resourceList-Goals.json");
+const habits = require("./resourceList-Habits.json");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+
+  app.get("/about", (req, res) => {
+    res.json(about);
+  });
+
+app.get("/timeManagement", (req, res) => {
+    res.json(timeManagement);
+  });
+
+  app.get("/goals", (req, res) => {
+    res.json(goals);
+  });
+
+  app.get("/habits", (req, res) => {
+    res.json(habits);
+  });
+  
+  const PORT = process.env.PORT || 4000;
+  
+  app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
